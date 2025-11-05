4aacdbe574de320b53ffb81e50bcc9b752a83cd1
beyzanur242002 <146854092+beyzanur242002@users.noreply.github.com>
2025-11-05 22:03:00 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,43 @@
 // JavaScript Document
 console.log("hi");
+
+
+//  de menu-button en sla die op in een variabele
+var openButton = document.querySelector("header > button");
+
+// de menu-button luisteren naar kliks en voer dan een functie uit
+openButton.onclick = openMenu;
+
+// de functie een class toe aan de nav
+function openMenu() {  
+  // zoek de nav op
+  var deNav = document.querySelector("nav");
+  // voeg class toe aan nav
+  deNav.classList.add("toonMenu");
+}
+
+var sluitButton = document.querySelector("nav > button");
+
+// laat die button luisteren naar kliks
+sluitButton.onclick = sluitMenu;
+
+// in de functie verwijder de class van de nav
+function sluitMenu() {
+  var deNav = document.querySelector("nav");
+  deNav.classList.remove("toonMenu");
+}
+
+
+
+
+/**********************************/
+/* menu sluiten met escape */
+/**********************************/
+window.onkeydown = handleKeydown;
+
+function handleKeydown(event) {
+  if (event.key == "Escape") {
+    var deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+  }
+}
